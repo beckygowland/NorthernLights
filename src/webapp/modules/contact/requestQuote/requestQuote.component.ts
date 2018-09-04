eeb25d1b57feda7b850b993326630177c0ffaab9
becky g <beckygowland@gmail.com>
2018-09-04 18:32:51 -0400

    Cleanup look

--- src/webapp/modules/contact/requestQuote/requestQuote.component.ts
+++ src/webapp/modules/contact/requestQuote/requestQuote.component.ts
@@ -1,5 +1,4 @@
-import { Component, ViewEncapsulation, Input, ElementRef } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { Component } from '@angular/core';
 import { ProductItem, Products } from '../../products/productsConstants';
 
  @Component({
